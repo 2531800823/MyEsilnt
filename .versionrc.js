982c03166eb81538a04e8d1611793f66803eb89b
liushipeng <liushipeng@bytedance.com>
2022-08-13 02:00:27 +0800

    fix: 添加changelog

--- .versionrc.js
+++ .versionrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  header: "CHANGELOG ！！",
   types: [
     { type: 'feat', section: '✨Features | 新功能' },
     { type: 'fix', section: '🐛 Bug Fixes | Bug 修复' },
@@ -10,8 +11,8 @@ module.exports = {
     { type: 'test', section: '✅ Tests | 测试' },
     { type: 'revert', section: '⏪ Revert | 回退' },
     { type: 'build', section: '📦‍ Build System | 打包构建' },
-    { type: 'chore', section: '🚀 Chore | 构建/工程依赖/工具' },
-    { type: 'ci', section: '👷 Continuous Integration | CI 配置' }
+    { type: 'chore', section: '🚀 Chore | 构建/工程依赖/配置' },
+    { type: 'ci', section: '👷 Continuous Integration | CI 部署' }
   ]
 }
 
